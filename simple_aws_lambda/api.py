ab4181605df31ee27ea815c19eab96064b82f632
Sanhe Hu <MacHu-GWU@users.noreply.github.com>
2025-09-03 01:20:44 -0400

    Expose grant/revoke layer access in API
    Added grant and revoke functions for AWS account or organization Lambda layer version access to the public API. Updated tests to cover new API exports. Bumped package version to 0.2.1.

--- simple_aws_lambda/api.py
+++ simple_aws_lambda/api.py
@@ -11,3 +11,5 @@ from .client import list_layer_versions
 from .client import get_layer_version
 from .recipe import get_latest_layer_version
 from .recipe import cleanup_old_layer_versions
+from .recipe import grant_aws_account_or_aws_organization_lambda_layer_version_access
+from .recipe import revoke_aws_account_or_aws_organization_lambda_layer_version_access
--- tests/test_api.py
+++ tests/test_api.py
@@ -15,6 +15,9 @@ def test():
     _ = api.list_layer_versions
     _ = api.get_layer_version
     _ = api.get_latest_layer_version
+    _ = api.cleanup_old_layer_versions
+    _ = api.grant_aws_account_or_aws_organization_lambda_layer_version_access
+    _ = api.revoke_aws_account_or_aws_organization_lambda_layer_version_access
 
 
 if __name__ == "__main__":
